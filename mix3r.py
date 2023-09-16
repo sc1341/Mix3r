dc52a1d8bfb9ceec612e774e8d35af852a5c99d0
sc1341 <33675337+sc1341@users.noreply.github.com>
2023-09-16 10:46:08 -0400

    Update mix3r.py
    Added a few lines of other mixes, improved help message.

--- mix3r.py
+++ mix3r.py
@@ -9,7 +9,7 @@ import string
 import os
 
 separators = ['-','.',',','*', '_', '=', '+', "@"]
-special_chars = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','`','~']
+special_chars = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','`','~', '{', '}','|', '?', '<', '>', ':', ';']
 
 def random_case(s):
     return ''.join(random.choice([str.upper, str.lower])(ch) for ch in s)
@@ -44,6 +44,10 @@ def process_words(file):
                 print_if_not_empty(line.capitalize())  # Capitalize first letter
                 print_if_not_empty(random_case(line)) # random case 
                 print_if_not_empty(line[::-1])  # Reverse
+                print_if_not_empty(''.join(ch.upper() if ch.islower() else ch.lower() for ch in line))
+                wrapped_lines = [char*n + line + char*n for char in special_chars for n in range(1, 3)]  # Adjust range to increase the number of wrapped characters
+                for wrapped in wrapped_lines:
+                     print_if_not_empty(wrapped) # print out results of previous operation 
                 print_if_not_empty(''.join(ch for ch in line if ch not in 'aeiouAEIOU'))  # No vowels
                 print_if_not_empty(''.join(ch * 2 for ch in line))  # Double letters
                 print_if_not_empty(''.join(ch + random.choice(string.ascii_letters) for ch in line))  # Random char between
@@ -85,7 +89,28 @@ def capitalize_range(word, start, end):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python3 mix3r.py [wordlist directory or wordlist]")
+        print("""mix3r.py: A Powerful Wordlist Manipulator
+
+Description:
+    mix3r.py is designed to take wordlists and apply various string manipulations 
+    to generate potential password candidates. Whether you're conducting a password 
+    cracking exercise or just want to see the myriad ways a string can be transformed, 
+    mix3r.py provides a comprehensive set of operations.
+
+Features:
+    1. Basic transformations: upper case, lower case, capitalize, reverse.
+    2. Advanced manipulations: leet speak conversion, random case application, vowel/consonant stripping.
+    3. Numeric augmentations: prepend/append years, append random numbers.
+    4. Special character operations: wrap strings with special characters, replace spaces with separators.
+    5. Predictive manipulations: guessed word boundary capitalization, pattern-based string repetition.
+
+Usage:
+    python3 mix3r.py [wordlist directory or wordlist]
+
+Note:
+    Output is sent to stdout. You can redirect it to a file or pipe it directly into tools like hashcat.
+    This is designed to be used in conjunction with a hashcat rule set, such as OneRuleToRuleThemAll. (https://github.com/NotSoSecure/password_cracking_rules)
+""")
         sys.exit()
     if os.path.isdir(sys.argv[1]):
         for file in os.listdir(sys.argv[1]):

c2a05f4fea2601b5fd3fd64c53603399c1a51147
orange <33675337+sc1341@users.noreply.github.com>
2024-02-03 12:01:54 -0500

    Updated docs

--- mix3r.py
+++ mix3r.py
@@ -2,7 +2,7 @@
 """
 mix3r.py - Mixes wordlists into common patterns that have been seen in real-world pentests.
 By: sc1341
-Last updated: January 13, 2024
+Last updated: February 2, 2024
 """
 
 import sys
@@ -16,7 +16,17 @@ special_chars = ['!','@','#','$','%','^','&','*','(',')','_','-','+','=','`','~'
 def random_case(s):
     return ''.join(random.choice([str.upper, str.lower])(ch) for ch in s)
 
-def simplified_leet_speak(word):
+def simplified_leet_speak(word) -> str:
+    """
+    Convert a word into simplified leet speak.
+
+    Args:
+        word (str): The word to convert.
+
+    Returns:
+        str: The word converted into simplified leet speak.
+
+    """
     leet_dict = {
         'a': ['@', '4'],
         'e': ['3'],
@@ -31,6 +41,17 @@ def simplified_leet_speak(word):
     }
 
     def generate_variations(char):
+        """
+        Generate variations of a character using leet_dict.
+
+        Args:
+            char (str): The character to generate variations for.
+
+        Returns:
+            list: A list of variations of the character. If the character is not found in leet_dict,
+                  the list will contain only the original character.
+
+        """
         return leet_dict.get(char.lower(), [char])
 
     variations = [generate_variations(char) for char in word]
@@ -76,8 +97,7 @@ def process_words(file):
                 print_if_not_empty(''.join(ch for ch in line if ch.lower() not in 'bcdfghjklmnpqrstvwxyz'))  # No consonants
                 print_if_not_empty(line[2:] + line[:2])  # Rotate by 2
                 guess_word_boundary_capitalize(line)  # Guess word boundary and capitalize
-                simplified_leet_speak(line)
-                # Add different separators if necessary 
+                simplified_leet_speak(line) # Leet speak
                 if ' ' in line:
                     for sep in separators:
                         print_if_not_empty(line.replace(' ', sep))
@@ -85,9 +105,14 @@ def process_words(file):
                 for i in range(0, 8):
                      print_if_not_empty(capitalize_range(line, 0, i))
 
-def print_if_not_empty(s, min_len=3, max_len=100):
+def print_if_not_empty(s, min_len=3, max_len=100) -> None:
     """
-    If the string s is not empty, then send to stdout. This is to prevent junk output.
+    Print the string s to stdout if it is not empty and meets the length criteria.
+
+    Args:
+        s (str): The string to be printed.
+        min_len (int, optional): The minimum length of the string. Defaults to 3.
+        max_len (int, optional): The maximum length of the string. Defaults to 100.
     """
     if len(s) > max_len or len(s) < min_len:
         return
@@ -96,9 +121,15 @@ def print_if_not_empty(s, min_len=3, max_len=100):
     if s.strip():
         print(s)
 
-def guess_word_boundary_capitalize(s):
+def guess_word_boundary_capitalize(s) -> None:
     """
     Guesses a word boundary from the input and capitalizes it in several iterations. For example, hotcrossbuns = hotCrossbuns, hotcRossbuns, etc.
+
+    Parameters:
+    - s: The input string to guess the word boundary and capitalize it.
+
+    Returns:
+    - None
     """
     for avg_word_length in range(2, 12):  # We will guess with avg_word_length from range
         if len(s) <= avg_word_length:
@@ -108,9 +139,17 @@ def guess_word_boundary_capitalize(s):
             print_if_not_empty(''.join(parts))
 
 
-def capitalize_range(word, start, end):
+def capitalize_range(word, start, end) -> str:
     """
-    Capitalizes a word from a specific range. 
+    Capitalizes a specific range of characters in a word.
+
+    Args:
+        word (str): The word to be capitalized.
+        start (int): The starting index of the range.
+        end (int): The ending index of the range.
+
+    Returns:
+        str: The word with the specified range of characters capitalized.
     """
     if len(word) < end:
         return word
@@ -120,29 +159,30 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("""mix3r.py: A Powerful Wordlist Manipulator
 
-Description:
-    mix3r.py is designed to take wordlists and apply various string manipulations 
-    to generate potential password candidates. Whether you're conducting a password 
-    cracking exercise or just want to see the myriad ways a string can be transformed, 
-    mix3r.py provides a comprehensive set of operations.
+        Description:
+            mix3r.py is designed to take wordlists and apply various string manipulations 
+            to generate potential password candidates. Whether you're conducting a password 
+            cracking exercise or just want to see the myriad ways a string can be transformed, 
+            mix3r.py provides a comprehensive set of operations.
 
-Features:
-    1. Basic transformations: upper case, lower case, capitalize, reverse.
-    2. Advanced manipulations: leet speak conversion, random case application, vowel/consonant stripping.
-    3. Numeric augmentations: prepend/append years, append random numbers.
-    4. Special character operations: wrap strings with special characters, replace spaces with separators.
-    5. Predictive manipulations: guessed word boundary capitalization, pattern-based string repetition.
+        Features:
+            1. Basic transformations: upper case, lower case, capitalize, reverse.
+            2. Advanced manipulations: leet speak conversion, random case application, vowel/consonant stripping.
+            3. Numeric augmentations: prepend/append years, append random numbers.
+            4. Special character operations: wrap strings with special characters, replace spaces with separators.
+            5. Predictive manipulations: guessed word boundary capitalization, pattern-based string repetition.
 
-Usage:
-    python3 mix3r.py [wordlist directory or wordlist]
+        Usage:
+            python3 mix3r.py [wordlist directory or wordlist]
 
-Note:
-    Output is sent to stdout. You can redirect it to a file or pipe it directly into tools like HashCat.
-    This is designed to be used in conjunction with a hashcat rule set, such as OneRuleToRuleThemAll to ensure enough input is given to HashCat. (https://github.com/NotSoSecure/password_cracking_rules)
-""")
+        Note:
+            Output is sent to stdout. You can redirect it to a file or pipe it directly into tools like HashCat.
+            This is designed to be used in conjunction with a hashcat rule set, such as OneRuleToRuleThemAll to ensure enough input is given to HashCat. (https://github.com/NotSoSecure/password_cracking_rules)
+        """)
         sys.exit()
+        
     if os.path.isdir(sys.argv[1]):
         for file in os.listdir(sys.argv[1]):
-            process_words(sys.argv[1] + file)
+            process_words(os.path.join(sys.argv[1], file))
     else:
         process_words(sys.argv[1])
